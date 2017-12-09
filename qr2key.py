4b950f2af1f0fd7c82bba0c3de3a730391dc81b5
Marco Zollinger <marco_zollinger_17@hotmail.com>
2017-12-09 22:18:15 +0100

    fixed indentation

--- qr2key.py
+++ qr2key.py
@@ -9,21 +9,21 @@ count_qrcodes = 0
 count_privkeys = 0
 
 with open('./keylist.txt', 'a') as key_list:
-   print('scanning PNG images for QR codes with bitcoin private keys...')
-   for png_path in glob.glob('./qrbooty/*.png'):
-      with open(png_path, 'rb') as png_file:
-         count_images += 1
-         image = Image.open(png_file)
-         image.load()
-         codes = zbarlight.scan_codes('qrcode', image)
-         for code in (codes or []):
-            count_qrcodes += 1
-            # TODO: match only BASE58 chars
-            if (re.match(r'5(H|J|K).{49}$', code) or   # match private key (WIF, uncompressed pubkey): length 51, BASE58
-                re.match(r'(K|L).{51}$', code) or      # match private key (WIF, compressed pubkey): length 52, BASE58
-                re.match(r'S(.{21}|.{29})$', code)):   # match mini private key: length 22 (deprecated) or 30, BASE58
-               count_privkeys += 1
-               key_list.write(code + '\n')
-               print('potential booty found!: %s' % code)
+    print('scanning PNG images for QR codes with bitcoin private keys...')
+    for png_path in glob.glob('./qrbooty/*.png'):
+        with open(png_path, 'rb') as png_file:
+            count_images += 1
+            image = Image.open(png_file)
+            image.load()
+            codes = zbarlight.scan_codes('qrcode', image)
+            for code in (codes or []):
+                count_qrcodes += 1
+                if ((re.match(r'5(H|J|K).{49}$', code) or        # match private key (WIF, uncompressed pubkey): length 51, BASE58
+                     re.match(r'(K|L).{51}$', code) or           # match private key (WIF, compressed pubkey): length 52, BASE58
+                     re.match(r'S(.{21}|.{29})$', code)) and     # match mini private key: length 22 (deprecated) or 30, BASE58
+                     re.match(r'[1-9A-HJ-NP-Za-km-z]+', code)):  # match only BASE58
+                    count_privkeys += 1
+                    key_list.write(code + '\n')
+                    print('potential booty found!: %s' % code)
    print('qr2key done. scanned %s images, with %s QR codes containing %s bitcoin private keys' % (count_images, count_qrcodes, count_privkeys))
    print('saved private keys to keylist.txt')
