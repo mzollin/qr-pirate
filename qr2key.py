97e0f909f565d1e96e14a6bd9ca1d539febb2a23
Marco Zollinger <marco_zollinger_17@hotmail.com>
2017-12-11 00:34:09 +0100

    decode byte object to string for python3 compatibility

--- qr2key.py
+++ qr2key.py
@@ -17,10 +17,11 @@ with open('./keylist.txt', 'a') as key_list:
             image.load()
             codes = zbarlight.scan_codes('qrcode', image)
             for code in (codes or []):
+                code = code.decode('ascii', errors='replace')
                 count_qrcodes += 1
-                if ((re.match(r'5(H|J|K).{49}$', code) or      # match private key (WIF, uncompressed pubkey): length 51, BASE58
-                   re.match(r'(K|L).{51}$', code) or           # match private key (WIF, compressed pubkey): length 52, BASE58
-                   re.match(r'S(.{21}|.{29})$', code)) and     # match mini private key: length 22 (deprecated) or 30, BASE58
+                if ((re.match(r'5(H|J|K).{49}$', code) or      # match private key (WIF, uncompressed pubkey) with length 51
+                   re.match(r'(K|L).{51}$', code) or           # match private key (WIF, compressed pubkey) with length 52
+                   re.match(r'S(.{21}|.{29})$', code)) and     # match mini private key with length 22 (deprecated) or 30
                    re.match(r'[1-9A-HJ-NP-Za-km-z]+', code)):  # match only BASE58
                     count_privkeys += 1
                     key_list.write(code + '\n')
