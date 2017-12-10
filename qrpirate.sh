9ed0fabad2950ca7d18bed1143d8e9d91eb5be96
Marco Zollinger <marco@freelabs.space>
2017-12-10 01:29:56 +0100

    remove duplicate debug output

--- qrpirate.sh
+++ qrpirate.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # crawling search engines for images matching description
 ./qrcrawler.py "$1"
-printf "crawling done.\n\n"
 
 for pathname in ./qrbooty/google/*; do filename=$(basename "$pathname"); mv "$pathname" "./qrbooty/google_$filename"; done
 for pathname in ./qrbooty/bing/*; do filename=$(basename "$pathname"); mv "$pathname" "./qrbooty/bing_$filename"; done
