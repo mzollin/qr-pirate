33a7726e2d7ffe4378b3485dd2fcce62a94e4d2f
Marco Zollinger <marco@freelabs.space>
2018-01-07 10:40:36 -0500

    added automatic wallet balance checking

--- qr2key.py
+++ qr2key.py
@@ -1,18 +1,20 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python2
 from PIL import Image
+from pybitcoin import BitcoinPrivateKey
+import requests
 import zbarlight
 import glob
 import re
 
-count_images = 0
-count_qrcodes = 0
-count_privkeys = 0
+counter_images = 0
+counter_qrcodes = 0
+counter_privkeys = 0
 
 with open('./keylist.txt', 'a') as key_list:
-    print('scanning images for QR codes with bitcoin private keys...')
+    print("scanning images for QR codes with bitcoin private keys...")
     for image_path in glob.glob('./qrbooty/*.*'):
         with open(image_path, 'rb') as image_file:
-            count_images += 1
+            counter_images += 1
             image = Image.open(image_file)
             try:
                 image.load()
@@ -24,14 +26,21 @@ with open('./keylist.txt', 'a') as key_list:
                 print("Could not decode: {}".format(e))
             for code in (codes or []):
                 code = code.decode('ascii', errors='replace')
-                count_qrcodes += 1
+                counter_qrcodes += 1
                 if ((re.match(r'5(H|J|K).{49}$', code) or      # match private key (WIF, uncompressed pubkey) with length 51
                    re.match(r'(K|L).{51}$', code) or           # match private key (WIF, compressed pubkey) with length 52
                    re.match(r'S(.{21}|.{29})$', code)) and     # match mini private key with length 22 (deprecated) or 30
                    re.match(r'[1-9A-HJ-NP-Za-km-z]+', code)):  # match only BASE58
-                    count_privkeys += 1
-                    key_list.write(code + '\n')
-                    print('potential booty found!: %s' % code)
-    print('qr2key done. scanned %s images, with %s QR codes containing %s bitcoin private keys' % (count_images, count_qrcodes, count_privkeys))
-    print('saved private keys to keylist.txt')
-   
+                    counter_privkeys += 1
+                    try:
+                        priv_key = BitcoinPrivateKey(code)
+                        pub_addr = priv_key.public_key().address()
+                        req = requests.get('https://blockchain.info/q/addressbalance/{}?confirmations=1'.format(pub_addr))
+                        key_list.write(code + '\n')
+                        print("booty found!: {} satoshi contained in key {}".format(req.json(), code))
+                    except (AssertionError, IndexError):
+                        pass
+                    except ValueError as e:
+                        print("Value Error: {}".format(e))
+    print("qr2key done. scanned {} images, with {} QR codes containing {} bitcoin private keys".format(count_images, count_qrcodes, count_privkeys))
+    print("saved private keys to keylist.txt")
