1eb3ed3b40338b56c5b0734bae7b035d5f4f69a1
Marco Zollinger <marco@freelabs.space>
2018-01-11 01:14:56 +0100

    add license header

--- qrpirate.sh
+++ qrpirate.sh
@@ -1,3 +1,6 @@
+# Copyright (c) 2018 Marco Zollinger
+# Licensed under MIT, the license file shall be included in all copies
+
 #!/bin/bash
 # crawling search engines for images matching description
 ./qrcrawler.py "$1"
